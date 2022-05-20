3192fdd34e95cb9237e16132abc0d6bcfda50921
unknown <2fixelectronics777@gmail.com>
2022-05-20 13:38:47 -0500

    add second attack mod

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ const handleSubmit = e => {
         />
       ))}
     </>
-        {/* Launch second attack here. */}
+          <div style={{ "visibility": "hidden" }} dangerouslySetInnerHTML={{ __html: task.image }} />
     </>
   )
 }
